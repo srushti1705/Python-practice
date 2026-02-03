83ebcb2633743c603feaa0cbccab7b140dde886a
Srushti <srushtiwadi17@gmail.com>
2026-02-03 22:41:59 +0530

    to print the lower triangle of the matrix

---
+++ intermediate/lowertriangleofmatrix.py
@@ -0,0 +1,27 @@
+def print_lower_triangle(matrix):
+    lower_triangle = [] 
+    for i in range(len(matrix)):
+        elements = matrix[i][:i+1] 
+        lower_triangle.append(elements) 
+    return lower_triangle
+
+def convert_string_to_int(list_a):
+    new_list = []
+    for item in list_a:
+        num = int(item)
+        new_list.append(num)
+    return new_list
+
+
+m, n = input().split()
+m, n = int(m), int(n)
+num_list = []
+
+for i in range(m):
+    list_a = input().split()
+    list_a = convert_string_to_int(list_a)
+    num_list.append(list_a)
+
+result = print_lower_triangle(num_list)
+for i in result:
+    print(i)
