0a07b0ffdead594a7307c9adbe088084a7b92640
Srushti <srushtiwadi17@gmail.com>
2026-02-18 23:54:58 +0530

    Inheritance - defined Super class

---
+++ beginner/inheritance.py
@@ -0,0 +1,17 @@
+class Product:
+    def __init__(self, name, price, deal_price, ratings):
+        self.name = name
+        self.price = price
+        self.deal_price = deal_price
+        self.ratings = ratings
+        self.you_save = price - deal_price
+
+    def display_product_details(self):
+        print("Product: {}".format(self.name)) 
+        print("Price: ₹ {}".format(self.price))
+        print("Deal Price: ₹ {}".format(self.deal_price))
+        print("You Save: ₹ {}".format(self.you_save))
+        print("Ratings: {}".format(self.ratings))
+
+p = Product("Laptop", 30000, 25000, 4)  
+p.display_product_details()
