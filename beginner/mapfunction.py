4486d1837bbe4ca733c4a3eda49c43175e488086
Srushti <srushtiwadi17@gmail.com>
2026-02-20 21:43:06 +0530

    Added a new small program to use map() function

--- beginner/mapfunction.py
+++ beginner/mapfunction.py
@@ -4,4 +4,10 @@ def square(n):
 numbers = [1, 2, 3, 4, 5] 
 result = map(square, numbers) 
 numbers_squared = list(result) 
-print(numbers_squared)
+print(numbers_squared) 
+
+line = input("Enter numbers separated by space: ") 
+str_nums = line.split()
+map_obj = map(int, str_nums)
+numbers = list(map_obj)
+print(numbers) 
