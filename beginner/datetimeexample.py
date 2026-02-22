b507e6c6a465c9ca1b85f801619c491ef86e3f52
Srushti <srushtiwadi17@gmail.com>
2026-02-22 22:29:28 +0530

    feat: add datetime module example

---
+++ beginner/datetimeexample.py
@@ -0,0 +1,10 @@
+import datetime
+
+date_obj = datetime.date(2020, 4, 3) 
+print(date_obj)
+date_obj2 = datetime.date.today() 
+print(date_obj2)
+time_obj = datetime.time(11, 34, 56) 
+print(time_obj)
+date_time_obj = datetime.datetime(2018, 11, 28, 10, 15, 26) 
+print(date_time_obj)
